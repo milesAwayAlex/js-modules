2dd50ccf6a3fe317f82180d6f2ba395274f6123e
Alex M <milesAwayAlex@gmail.com>
2021-11-28 19:38:24 -0800

    Module caching

--- commonJS-modules/c-import-main.js
+++ commonJS-modules/c-import-main.js
@@ -1,3 +1,10 @@
 // to demonsrate the difference between running a module directly
 // and requiring it from a different file
 require('./c-main-module.js');
+
+// modules are cached after the first time they are loaded
+// provided `require.cache` is not modified, multiple calls
+// to `require('foo') will not cause the module code to be
+// executed multiple times. This allows transitive dependencies
+// to be loaded even when they would cause cycles.
+require('./c-main-module.js');
