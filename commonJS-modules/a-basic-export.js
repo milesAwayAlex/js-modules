178ca28e5cebb32fd24a70b6bfdb8babba6a20c5
Alex M <milesAwayAlex@gmail.com>
2021-11-28 18:34:49 -0800

    Module local variables

--- commonJS-modules/a-basic-export.js
+++ commonJS-modules/a-basic-export.js
@@ -1,5 +1,7 @@
+// PI is private to the module
 const { PI } = Math;
 
+// exports are exported
 exports.area = (r) => PI * r ** 2;
 
 exports.circumference = (r) => 2 * PI * r;
--- commonJS-modules/a-basic-import.js
+++ commonJS-modules/a-basic-import.js
@@ -1,2 +1,3 @@
 const circle = require('./a-basic-export.js');
+
 console.log(`The area of a circle of radius 4 is ${circle.area(4)}`);
